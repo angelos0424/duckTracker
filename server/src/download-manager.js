e7929eeafb8181d619b99fbe07d2ad00585c6c0d
twkim <angelos0424@gmail.com>
2025-09-26 19:15:37 +0900

    Update download-manager.js

--- server/src/download-manager.js
+++ server/src/download-manager.js
@@ -243,9 +243,9 @@ class DownloadManager extends EventEmitter {
       if (!fs.existsSync(cookieFilePath)) {
         throw new Error(`Cookie file not found: ${cookieFilePath}`);
       }
-      cookieSetting = `--cookies '${cookieFilePath}'`;
+      cookieSetting = `--cookies ${cookieFilePath}`;
     } else if (chromePath) {
-      cookieSetting = `--cookies-from-browser '${chromePath}'`;
+      cookieSetting = `--cookies-from-browser ${chromePath}`;
     }
 
     const args = [
