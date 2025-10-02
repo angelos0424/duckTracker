424ded60a7c124477e87d459bd129898d2103e2c
twkim <angelos0424@gmail.com>
2025-10-02 15:47:22 +0900

    delete design source

--- server/newDesign/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { DownloadManager } from "./components/DownloadManager";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <DownloadManager />
-    </div>
-  );
-}
